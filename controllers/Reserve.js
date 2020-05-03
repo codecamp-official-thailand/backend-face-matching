372367b12398da80dcf0f070fad240ef0e3054d7
spk-cpsamut <taresupakon@gmail.com>
2020-05-03 23:48:19 +0700

    finsh spint 1 backend-face-matching (+1 squashed commits)
    Squashed commits:
    
    [01397ea] admin feature in process

--- controllers/Reserve.js
+++ controllers/Reserve.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const { QueryTypes } = require("sequelize");
 
 const createNewReserve = async (req, res) => {
   const requestId = Number(req.query.request_id);
@@ -13,6 +14,7 @@ const createNewReserve = async (req, res) => {
       request_amount: targetRequest.request_amount - reserveAmount,
       reserve_amount: targetRequest.reserve_amount + reserveAmount,
     });
+    console.log(`user id${req.user.id}`);
 
     await db.Reserve.create({
       amount: reserveAmount,
@@ -116,9 +118,57 @@ const deleteReserve = async (req, res) => {
   }
 };
 
+const getReserveAndMakerName = async (req, res) => {
+  const reserveAndMakerName = await db.sequelize.query(
+    `SELECT  RS.id,RS.amount,RS.createdAt,firstname,phone_no,MS.name,MS.line_id FROM requests RQ INNER JOIN reserves RS ON RQ.id = RS.request_id AND RS.status = "RESERVE" INNER JOIN makers M ON RS.maker_id = M.id INNER JOIN medical_staffs MS ON RQ.medical_staff_id = MS.id
+   `,
+    { type: QueryTypes.SELECT }
+  );
+
+  res.status(200).send(reserveAndMakerName);
+};
+
+const deleteReserveForAdmin = async (req, res) => {
+  const reserveId = Number(req.params.id);
+
+  const targetReserve = await db.Reserve.findOne({
+    where: { id: reserveId, status: "RESERVE" },
+  });
+
+  if (!targetReserve) {
+    res.status(404).send({ message: "Reserve not found" });
+  } else {
+    const targetRequest = await db.Request.findOne({
+      where: { id: Number(targetReserve.request_id) },
+    });
+
+    const t = await db.sequelize.transaction();
+
+    try {
+      await targetReserve.update({ status: "CANCELED" }, { transaction: t });
+      await targetRequest.update(
+        {
+          request_amount: targetRequest.request_amount + targetReserve.amount,
+          reserve_amount: targetRequest.reserve_amount - targetReserve.amount,
+        },
+        { transaction: t }
+      );
+
+      await t.commit();
+      res.status(200).send({ message: "DELETED" });
+    } catch (err) {
+      console.log(err);
+      await t.rollback();
+      res.status(400).send(err);
+    }
+  }
+};
+
 module.exports = {
   createNewReserve,
   getAllReservesByMakerIds,
   completedReserve,
   deleteReserve,
+  getReserveAndMakerName,
+  deleteReserveForAdmin,
 };
